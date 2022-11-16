85ddefa7a5670642ac519633b4afb2e0b3898efb
unknown <jenrolaolasupo@gmail.com>
2022-11-16 11:24:06 +0100

    Added stuff

--- src/Places/Places.js
+++ src/Places/Places.js
@@ -13,6 +13,18 @@ import Desert5 from '../Images/Frame 151 (4).png'
 import Desert6 from '../Images/Frame 151 (5).png'
 import Desert7 from '../Images/Frame 151 (6).png'
 import Desert8 from '../Images/Frame 151 (7).png'
+import Desert9 from '../Images/Frame 151 (8).png'
+import Desert10 from '../Images/Frame 151 (9).png'
+import Desert11 from '../Images/Frame 151 (10).png'
+import Desert12 from '../Images/Frame 151 (11).png'
+import Desert13 from '../Images/Frame 151 (12).png'
+import Desert14 from '../Images/Frame 151 (13).png'
+import Desert15 from '../Images/Frame 151 (14).png'
+import Desert16 from '../Images/Frame 151 (15).png'
+import Desert17 from '../Images/Frame 151 (16).png'
+import Desert18 from '../Images/Frame 151 (17).png'
+
+
 
 
 // import Desert2 from "../Images/Frame 151 (1).svg";
@@ -42,66 +54,6 @@ const Places = () => {
       </div>
       <div className="Meta2">
         <div className="Desert">
-          <div className="desert">
-            <img src={Desert1} alt="" className="Des" />
-            <div className="desertT">
-              <div>
-                <h3 className="King">Desert king</h3>
-                <p className="King">2345km away</p>
-              </div>
-              <div>
-                <h3 className="Night">1MBT per night</h3>
-                <p className="King">available for 2weeks stay</p>
-              </div>
-            </div>
-            <img src={Star} alt="" className="star" />
-          </div>
-
-          <div className="desert">
-            <img src={Desert2} alt="" className="Des" />
-            <div className="desertT">
-              <div>
-                <h3 className="King">Desert king</h3>
-                <p className="King">2345km away</p>
-              </div>
-              <div>
-                <h3 className="Night">1MBT per night</h3>
-                <p className="King">available for 2weeks stay</p>
-              </div>
-            </div>
-            <img src={Star} alt="" className="star" />
-          </div>
-
-          <div className="desert">
-            <img src={Desert3} alt="" className="Des" />
-            <div className="desertT">
-              <div>
-                <h3 className="King">Desert king</h3>
-                <p className="King">2345km away</p>
-              </div>
-              <div>
-                <h3 className="Night">1MBT per night</h3>
-                <p className="King">available for 2weeks stay</p>
-              </div>
-            </div>
-            <img src={Star} alt="" className="star" />
-          </div>
-
-          <div className="desert">
-            <img src={Desert4} alt="" className="Des" />
-            <div className="desertT">
-              <div>
-                <h3 className="King">Desert king</h3>
-                <p className="King">2345km away</p>
-              </div>
-              <div>
-                <h3 className="Night">1MBT per night</h3>
-                <p className="King">available for 2weeks stay</p>
-              </div>
-            </div>
-            <img src={Star} alt="" className="star" />
-          </div>
-
           <div className="desert">
             <img src={Desert5} alt="" className="Des" />
             <div className="desertT">
@@ -148,7 +100,7 @@ const Places = () => {
           </div>
 
           <div className="desert">
-            <img src={Desert1} alt="" className="Des" />
+            <img src={Desert10} alt="" className="Des" />
             <div className="desertT">
               <div>
                 <h3 className="King">Desert king</h3>
@@ -161,8 +113,9 @@ const Places = () => {
             </div>
             <img src={Star} alt="" className="star" />
           </div>
+
           <div className="desert">
-            <img src={Desert8} alt="" className="Des" />
+            <img src={Desert11} alt="" className="Des" />
             <div className="desertT">
               <div>
                 <h3 className="King">Desert king</h3>
@@ -192,7 +145,7 @@ const Places = () => {
           </div>
 
           <div className="desert">
-            <img src={Desert1} alt="" className="Des" />
+            <img src={Desert3} alt="" className="Des" />
             <div className="desertT">
               <div>
                 <h3 className="King">Desert king</h3>
@@ -207,7 +160,21 @@ const Places = () => {
           </div>
 
           <div className="desert">
-            <img src={Desert1} alt="" className="Des" />
+            <img src={Desert4} alt="" className="Des" />
+            <div className="desertT">
+              <div>
+                <h3 className="King">Desert king</h3>
+                <p className="King">2345km away</p>
+              </div>
+              <div>
+                <h3 className="Night">1MBT per night</h3>
+                <p className="King">available for 2weeks stay</p>
+              </div>
+            </div>
+            <img src={Star} alt="" className="star" />
+          </div>
+          <div className="desert">
+            <img src={Desert11} alt="" className="Des" />
             <div className="desertT">
               <div>
                 <h3 className="King">Desert king</h3>
@@ -222,7 +189,7 @@ const Places = () => {
           </div>
 
           <div className="desert">
-            <img src={Desert1} alt="" className="Des" />
+            <img src={Desert12} alt="" className="Des" />
             <div className="desertT">
               <div>
                 <h3 className="King">Desert king</h3>
@@ -237,7 +204,7 @@ const Places = () => {
           </div>
 
           <div className="desert">
-            <img src={Desert1} alt="" className="Des" />
+            <img src={Desert13} alt="" className="Des" />
             <div className="desertT">
               <div>
                 <h3 className="King">Desert king</h3>
@@ -252,7 +219,7 @@ const Places = () => {
           </div>
 
           <div className="desert">
-            <img src={Desert1} alt="" className="Des" />
+            <img src={Desert14} alt="" className="Des" />
             <div className="desertT">
               <div>
                 <h3 className="King">Desert king</h3>
@@ -267,7 +234,52 @@ const Places = () => {
           </div>
 
           <div className="desert">
-            <img src={Desert1} alt="" className="Des" />
+            <img src={Desert15} alt="" className="Des" />
+            <div className="desertT">
+              <div>
+                <h3 className="King">Desert king</h3>
+                <p className="King">2345km away</p>
+              </div>
+              <div>
+                <h3 className="Night">1MBT per night</h3>
+                <p className="King">available for 2weeks stay</p>
+              </div>
+            </div>
+            <img src={Star} alt="" className="star" />
+          </div>
+
+          <div className="desert">
+            <img src={Desert16} alt="" className="Des" />
+            <div className="desertT">
+              <div>
+                <h3 className="King">Desert king</h3>
+                <p className="King">2345km away</p>
+              </div>
+              <div>
+                <h3 className="Night">1MBT per night</h3>
+                <p className="King">available for 2weeks stay</p>
+              </div>
+            </div>
+            <img src={Star} alt="" className="star" />
+          </div>
+
+          <div className="desert">
+            <img src={Desert17} alt="" className="Des" />
+            <div className="desertT">
+              <div>
+                <h3 className="King">Desert king</h3>
+                <p className="King">2345km away</p>
+              </div>
+              <div>
+                <h3 className="Night">1MBT per night</h3>
+                <p className="King">available for 2weeks stay</p>
+              </div>
+            </div>
+            <img src={Star} alt="" className="star" />
+          </div>
+
+          <div className="desert">
+            <img src={Desert18} alt="" className="Des" />
             <div className="desertT">
               <div>
                 <h3 className="King">Desert king</h3>
